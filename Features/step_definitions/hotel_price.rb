8ef5b412b1bf6e38d76c0404766629b784d1e418
Andy Lyon <lyonaj@gmail.com>
2012-10-30 21:18:24 +0000

    removed extra right parentheses

--- Features/step_definitions/hotel_price.rb
+++ Features/step_definitions/hotel_price.rb
@@ -4,16 +4,16 @@ end
 
 Given /^I have entered a price "(.*?)"$/ do |arg1|
 	find_element(:css, "option[value=\"KN\"]").click
-    find_element(:name, "country")).select_by(:text, "Syria")
+    find_element(:name, "country").select_by(:text, "Syria")
     find_element(:css, "option[value=\"SY\"]").click
     find_element(:name, "country").click
-    find_element(:name, "country")).select_by(:text, "United Kingdom")
+    find_element(:name, "country").select_by(:text, "United Kingdom")
     find_element(:css, "option[value=\"GB\"]").click
     find_element(:name, "postCode").clear
     find_element(:name, "postCode").send_keys "ch1 4hz"
-    find_element(:name, "priceRange")).select_by(:text, "101-150")
-    find_element(:id, "arrival_dsd")).select_by(:text, "24")
-    find_element(:id, "arrival_dsm")).select_by(:text, "Nov")
+    find_element(:name, "priceRange").select_by(:text, "101-150")
+    find_element(:id, "arrival_dsd").select_by(:text, "24")
+    find_element(:id, "arrival_dsm").select_by(:text, "Nov")
  end
 
 When /^I click Search$/ do
